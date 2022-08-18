b2904f4894d934d86d78df6a53af2f2261a34c87
Julie V <julievitse91@gmail.com>
2022-08-18 11:30:04 +0200

    added file format verification in NewBill.js

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -15,17 +15,26 @@ export default class NewBill {
     this.billId = null
     new Logout({ document, localStorage, onNavigate })
   }
+
   handleChangeFile = e => {
     e.preventDefault()
+    const fileInput = this.document.querySelector(`input[data-testid="file"]`); // récupère l'input file
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
+
+    const fileExtension = fileName.split('.').pop(); // récupère seulement l'extension du fichier
+
+    const fileFormats = ["jpg", "jpeg", "png"]; // défini formats autorisés
+
     const formData = new FormData()
     const email = JSON.parse(localStorage.getItem("user")).email
     formData.append('file', file)
     formData.append('email', email)
 
-    this.store
+    //si le format est bon, on créé la nouvelle note de frais
+    if (fileFormats.includes(fileExtension)) {
+      this.store
       .bills()
       .create({
         data: formData,
@@ -39,7 +48,15 @@ export default class NewBill {
         this.fileUrl = fileUrl
         this.fileName = fileName
       }).catch(error => console.error(error))
+    } 
+    //sinon on affiche une alerte et on vide l'input, aucune note de frais n'est créée
+    else {
+      alert("Le format du fichier sélectionné est interdit. Veuillez sélectionner un fichier png, jpg ou jpeg.");
+      fileInput.value = "";
+    }
+    
   }
+
   handleSubmit = e => {
     e.preventDefault()
     console.log('e.target.querySelector(`input[data-testid="datepicker"]`).value', e.target.querySelector(`input[data-testid="datepicker"]`).value)
